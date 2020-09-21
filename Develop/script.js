00ed262b2fce3c99e968a4ae8a9220b136b36569
Brie Bencich <bbencich@hotmail.com>
2020-09-21 04:11:37 +0300

    prompt for the beginning of the password generator added

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,25 @@
 // Assignment code here
 
 
+// Prompts stating the criteria for the Password.
+var criteria = window.alert("Please select the correct criteria you wish, to generate your new password. Password must be between 8-128 characters, with either lowercase, uppercase, specaial, or numeric chatacters. ");
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
+  
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
